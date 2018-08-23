670f51b91a714a0c48feb89a3c8e11af6db22adc
pedrobellotti <pedro177@gmail.com>
2018-08-23 18:12:04 -0300

    Aula 4

--- src/java/UFJF/NewServlet.java
+++ src/java/UFJF/NewServlet.java
@@ -5,8 +5,13 @@
  */
 package UFJF;
 
+import java.sql.Statement;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,13 +22,6 @@ import javax.servlet.http.HttpServletResponse;
  * @author pedro
  */
 public class NewServlet extends HttpServlet {
-    String usuario;
-    String senha;
-    public void init(){
-        usuario = getInitParameter("usuario");
-        senha = getInitParameter("senha");
-    }
-
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -37,15 +35,68 @@ public class NewServlet extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            String usu = request.getParameter("usuario");
-            String pw = request.getParameter("senha");
-            
-            /* Encaminha para a pagina de menu do sistema. */
-            if(usuario.equals(usu) && senha.equals(pw)){
-                request.getRequestDispatcher("menu.html").forward(request,response);
+            //Pega a senha do banco de dados
+            String JDBC_DRIVER = "org.apache.derby.jdbc.ClientDriver";
+            String DB_URL = "jdbc:derby://localhost:1527/dados";
+            //Credenciais na database
+            Connection conn = null;
+            Statement stmt = null;
+            String resp = null;
+            boolean result = false;
+          
+            //Usuario e senha enviados pelo formulario
+            String usuario = request.getParameter("usuario");
+            String senha = request.getParameter("senha");
+            //Tipo de resposta
+            try{
+                //Registrar driver JDBC
+                Class.forName(JDBC_DRIVER);
+                //Abre conexão
+                conn = DriverManager.getConnection(DB_URL, "nbuser", "abc1234");
+                //Faz query sql
+                stmt = conn.createStatement();
+                String sql;
+                sql = "SELECT usuario, senha FROM app.tabela where upper(usuario) = '"+usuario.toUpperCase() + "' and senha='"+senha+"'";
+                ResultSet rs = stmt.executeQuery(sql);
+                //Extrai os dados dos resultados
+                if(rs.next())
+                    result = true;
+                if(result)
+                    request.getRequestDispatcher("menu.html").forward(request,response);
+                else
+                    request.getRequestDispatcher("erro.html").forward(request,response);
+                rs.close();
+                stmt.close();
+                conn.close();
             }
-            else{
-                out.println("<p>Usuário e senha inválidos!</p>");
+            catch(SQLException e){
+                //Cuida dos erros
+                resp = e.getMessage();
+                throw new ServletException(e);
+            }
+            catch(Exception e){
+                //Erros do class.name
+                resp = e.getMessage();
+                throw new ServletException(e);
+            }
+            finally {
+                System.out.printf(resp);
+                //Fecha as conexoes
+                try{
+                    if(stmt != null)
+                        stmt.close();
+                }
+                catch (SQLException e){
+                    throw new ServletException(e);
+                }
+                try{
+                    if(conn != null)
+                        conn.close();
+                    
+                }
+                catch (SQLException e){
+                    throw new ServletException(e);
+                }
             }
         }
     }
